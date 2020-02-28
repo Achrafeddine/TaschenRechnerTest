83cb14c237e3c154a6d5eaa4c242c8258a0597f1
Achrafeddine Ahnin <achrafaaroui@gmail.com>
2020-02-28 17:24:58 +0100

    Initial commit

---
+++ Lernen/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lernen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            string[] Klass = new string[4];
+            Klass[0]="Nabil";
+            Klass[1] = "Achraf";
+            Klass[2] = "Moussa";
+
+            for(i=0; i<Klass.LongCount();i++) 
+            {
+                Console.WriteLine(Klass[i]);
+
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
